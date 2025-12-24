9252539117cb298d4d2c47ff41edaf0e33f38f8a
Vini P <viniuk.p@gmail.com>
2025-12-24 09:59:45 +0000

    providr updated for k8s cluster

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -39,19 +39,28 @@ provider "azurerm" {
 
 # Kubernetes provider
 # main.tf
-provider "kubernetes" {
+/*provider "kubernetes" {
   alias                  = "aks"
   host                   = module.aks.kube_config[0].host
   client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
   client_key             = base64decode(module.aks.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
 
+} */
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = module.aks.aks_name
+  resource_group_name = var.resource_group
+  depends_on          = [module.aks]
 }
 
+data "azurerm_client_config" "current" {}
 
-
-
-
+provider "kubernetes" {
+  alias                  = "aks"
+  host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  token                  = data.azurerm_client_config.current.access_token
+}
 
 # AzureAD provider
 provider "azuread" {}
