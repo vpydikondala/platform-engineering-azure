66e4e7fed364a2d3f1ffe4d19e28e3734b27663d
Vini P <viniuk.p@gmail.com>
2025-12-24 08:19:29 +0000

    changed k8s version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,14 +12,19 @@ terraform {
       version = "~> 2.0" 
     }
 
-    kubernetes = { 
+    kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0" 
+      version = ">= 2.25.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.8"   # example latest v2 version
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
     }
   }
 }
@@ -29,21 +34,21 @@ provider "azurerm" {
   features {} 
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-  # Enable OIDC login for GitHub Actions
-  use_oidc = true
+  use_oidc        = true
 }
 
-# Kubernetes provider (AKS)
 # Kubernetes provider
 # main.tf
 provider "kubernetes" {
-  config_path = local_file.kubeconfig.filename
+  host                   = module.aks.kube_config.host
+  client_certificate     = base64decode(module.aks.kube_config.client_certificate)
+  client_key             = base64decode(module.aks.kube_config.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config.cluster_ca_certificate)
 }
+
 
 
 # AzureAD provider
 provider "azuread" {}
 
-# Helm provider
-# No credentials are passed here — it will use the Kubernetes provider in helm_release
-provider "helm" {}
+
