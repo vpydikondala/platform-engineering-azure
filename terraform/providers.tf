01ff8cb0e92728c6522f5fa10b970ad7544dddd8
Vini P <viniuk.p@gmail.com>
2025-12-24 01:21:37 +0000

    updated providers for helm

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,38 +3,43 @@ terraform {
 
   required_providers {
     azurerm = {
-    source  = "hashicorp/azurerm"
-    version = ">= 3.64.0"
+      source  = "hashicorp/azurerm"
+      version = ">= 3.64.0"
     }
 
     azuread = { 
-     source = "hashicorp/azuread"
-     version = "~> 2.0" 
-     }
+      source  = "hashicorp/azuread"
+      version = "~> 2.0" 
+    }
+
     kubernetes = { 
-        source = "hashicorp/kubernetes"
-        version = "~> 2.0" 
-        }
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0" 
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8"   # example latest v2 version
+    }
   }
 }
 
+# Azure provider
 provider "azurerm" { 
-    features {} 
+  features {} 
 }
+
+# Kubernetes provider (AKS)
 provider "kubernetes" {
   host                   = module.aks.kube_config[0].host
   client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
   client_key             = base64decode(module.aks.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
-  }
+}
+
+# AzureAD provider
 provider "azuread" {}
 
-# Helm Provider
-provider "helm" {
- {
-    host                   = module.aks.kube_config[0].host
-    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(module.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
-  }
-}
+# Helm provider
+# No credentials are passed here — it will use the Kubernetes provider in helm_release
+provider "helm" {}
