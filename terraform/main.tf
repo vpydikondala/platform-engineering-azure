635b4bfbcf3a2c91398a8d7a92248a28f3030105
Vini P <viniuk.p@gmail.com>
2025-12-24 15:20:06 +0000

    new file created under secrretproviderclass"

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,21 +28,28 @@ resource "kubernetes_namespace_v1" "platform" {
 }
 # SecretProviderClass module
 module "secret_provider_class" {
-  source            = "./modules/k8s_platform/secretproviderclass"
-  keyvault_name     = module.keyvault.keyvault_name
-  tenant_id         = var.tenant_id
+  source             = "./modules/k8s_platform/secretproviderclass"
+  keyvault_name      = module.keyvault.keyvault_name
+  tenant_id          = data.azurerm_client_config.current.tenant_id
   platform_namespace = "platform-observability"
-  objects           = [
+
+  objects = [
     {
       objectName = "grafana-admin-password"
       objectType = "secret"
     }
   ]
-   # Ensure Terraform knows the dependency
- #depends_on = [module.keyvault,module.k8s_platform,kubernetes_namespace_v1.platform]
+
+  providers = {
+    kubernetes = kubernetes.aks
+  }
+
+  depends_on = [
+    module.keyvault,
+    module.k8s_platform,
+    kubernetes_namespace_v1.platform
+  ]
 }
-
-
 module "k8s_platform" {
  source        = "./modules/k8s_platform"
    providers = {
